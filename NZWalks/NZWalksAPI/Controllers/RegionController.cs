5e5b1af702d7c7a605f2a64e0a0063130cf711f4
Fernando Sustaita <fernandojsustaita@gmail.com>
2022-10-05 14:56:28 -0500

    region async endpoint

---
+++ NZWalks/NZWalksAPI/Controllers/RegionController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalksAPI.Models.Domain;
+using NZWalksAPI.Repositories;
+
+namespace NZWalksAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegionController : Controller
+    {
+        private readonly IRegionRepository regionRepository;
+        private readonly IMapper mapper;
+
+        public RegionController(IRegionRepository regionRepository, IMapper mapper)
+        {
+            this.regionRepository = regionRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllregions()
+        {
+            var regions = regionRepository.GetAll();
+            //var regionsDto = new List<Models.DTOs.RegionDto>();
+
+            //regions.ToList().ForEach(region =>
+            //{
+            //    var regionDto = new Models.DTOs.RegionDto()
+            //    {
+            //        Id = region.Id,
+            //        Name = region.Name,
+            //        Code = region.Code,
+            //        Area = region.Area,
+            //        Lat = region.Lat,
+            //        Long = region.Long,
+            //        Population = region.Population
+            //    };
+            //    regionsDto.Add(regionDto);
+            //});
+
+            var regionsDto = mapper.Map<Models.DTOs.RegionDto>(regions);
+
+            return Ok(regionsDto);
+        }
+    }
+}
